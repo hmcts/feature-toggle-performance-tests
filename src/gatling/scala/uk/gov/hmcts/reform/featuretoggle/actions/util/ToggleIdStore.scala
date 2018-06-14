cf8506865b6b2b8641655da511fd23caa9ba99b2
Rafal Kondratowicz <rkondr@gmail.com>
2018-06-14 14:17:58 +0100

    Just use a list

--- src/gatling/scala/uk/gov/hmcts/reform/featuretoggle/actions/util/ToggleIdStore.scala
+++ src/gatling/scala/uk/gov/hmcts/reform/featuretoggle/actions/util/ToggleIdStore.scala
@@ -7,12 +7,14 @@ import scala.util.Random
 
 object ToggleIdStore {
 
-  private val ids = mutable.SortedSet[String]()
+  private val ids = mutable.MutableList[String]()
 
   def addRandom() = {
     val id = "perf-test-" + UUID.randomUUID.toString
-    ids.add(id)
+    ids += id
+
     id
   }
-  def getRandom() = ids.toVector((new Random).nextInt(ids.size))
+
+  def getRandom() = ids((new Random).nextInt(ids.size))
 }
